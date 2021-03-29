8bb4a1446f2f47f267a6bda1fdd392046153278b
Chavanlak <chavanlak.p@ku.th>
2021-03-29 17:50:46 +0700

    k=k%10;

---
+++ 6220503198_Nearest Number.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int k,i,n,c;
+    scanf("%d",n);
+     for(j;j<=;j++)
+    for(i=k;k<=n;k++)
+    {
+        if(n%2==0)
+        {
+            k=k%10;
+            k=k+1;
+
+        }
+        else
+        printf("");
+
+    }
+}
