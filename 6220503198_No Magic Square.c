71b9b422932caac2ffc7a63a97deec0531821703
Chavanlak <chavanlak.p@ku.th>
2021-03-29 19:33:21 +0700

    for(num=2;row=0,col=s;num=max;num++))

--- 6220503198_No Magic Square.c
+++ 6220503198_No Magic Square.c
@@ -1,5 +1,20 @@
 #include<stdio.h>
 int main()
 {
+    int magic[10][10];
+    int size;
+    printf("Enter size : ");
+    scanf("%d",&size);
+    int s =size/2;
+    int max = size * size;
+    magic[0][s]=1;
+
+    int row;
+    int col;
+    int next_row;
+    int next_col;
+    int num ;
+
+    for(num=2;row=0,col=s;num=max;num++)
 
 }
