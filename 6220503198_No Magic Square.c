13c97e3219ead0b4e817e2d1c46e8f3e6cd0b9ac
Chavanlak <chavanlak.p@ku.th>
2021-03-29 19:51:17 +0700

    printf("%3d"magic[i][j]);

--- 6220503198_No Magic Square.c
+++ 6220503198_No Magic Square.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    int magic[10][10];
+    int magic[100][100];
     int size;
     printf("Enter size : ");
     scanf("%d",&size);
     int s =size/2;
-    int max = size * size;
+    int max = size*size;
     magic[0][s]=1;
 
     int row;
@@ -15,7 +15,9 @@ int main()
     int next_col;
     int num ;
 
-    for(num=2;row=0,col=s;num=max;num++)
+    for(num=2,row=0,col=s;num<=max;num++)
+    {
+
         if((col+1)<(size-1))
     {
         next_col =0;
@@ -32,5 +34,34 @@ int main()
     {
         next_row=row-1;
     }
-    if()
+    if(magic[next_row][next_col]>0)
+    {
+        if((row+1)>(size-1))
+            next_row = 0;
+    }
+
+
+    else
+    {
+        next_row=row+1;
+        next_col=col;
+
+    }
+
 }
+row=next_row;
+col=next_col;
+magic[row][col]=num;
+}
+    int i,j;
+    for(i=0;i<size;i++)
+{
+    for(j=0;j<size;j++)
+    {
+        printf("%3d"magic[i][j]);
+    }
+    printf("\n");
+}
+getch();
+}
+
